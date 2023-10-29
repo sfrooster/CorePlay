9bec84124f9bd7cb8fe8eae6f94495806e3f7db3
sfrooster <haanmd@gmail.com>
2023-10-29 03:13:45 -0700

    dep inj of svc

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,32 @@
+using static System.Security.Cryptography.RandomNumberGenerator;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton(new HuggingService());
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello  World!");
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/hugs", (Hug hug, HuggingService hugger) =>
+  Results.Ok(hugger.Hug(hug))
+);
 
 app.Run();
+
+public record Hug(string Name);
+
+public record Hugged(string Name, string Kind);
+
+public class HuggingService
+{
+  private readonly string[] _hugKinds = {
+    "Side Hug", "Bear Hug",
+    "Polite Hug", "Back Hug",
+    "Self Hug"
+  };
+
+  private string RandomKind =>
+    _hugKinds[GetInt32(0, _hugKinds.Length)];
+
+  public Hugged Hug(Hug hug)
+    => new(hug.Name, RandomKind);
+}
